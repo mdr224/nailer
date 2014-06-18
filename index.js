3be3bcf98a9f950fa89433ff485f94329fbb64e6
Michael Rosenfeld <michael.rosenfeld@originate.com>
2014-06-18 16:20:29 -0400

    Clean all dbs before starting.

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ var url = 'mongodb://heroku_app26443906:qjc89p63q5iicq4lqdnkmfehtb@ds033828.mong
 var collections = ['alias', 'identify', 'page', 'track'];
 var db = mongojs(url, collections);
 
+// NOTE: TEMPORARY
+db.alias.remove( {} );
 db.identify.remove( {} );
+db.page.remove( {} );
+db.track.remove( {} );
 
 // post requests (HTTP)
 app.post('/', function (req, res) {
